4b60fc204f17e41e43d5ad800a1a75dde9f057ab
patxi <patxiarana05@gmail.com>
2023-02-06 21:37:50 -0300

    first test

---
+++ index.js
@@ -0,0 +1,3 @@
+var usernmae = "patxi";
+var age = 33;
+console.log("hello wolrd my name is ".concat(usernmae, " and my age is ").concat(age, " year old"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+const usernmae = "patxi"
+const age = 33
+
+
+console.log(`hello wolrd my name is ${usernmae} and my age is ${age} year old`)
