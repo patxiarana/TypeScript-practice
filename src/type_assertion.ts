81f5be3297cc2b35a0c38f8cf9df948ab4681395
patxi <patxiarana05@gmail.com>
2023-02-10 18:16:19 -0300

    htlm assertions

--- src/type_assertion.ts
+++ src/type_assertion.ts
@@ -5,3 +5,7 @@ let channel : any = 'patxidev'
 //let channelStr = <string> channel ;
 
 let channelStr = channel as string 
+
+ // const myCanvas = document.getElementById('main') as HTMLCanvasElement ;
+
+ const myCanvas = <HTMLCanvasElement>document.getElementById('main') ;
