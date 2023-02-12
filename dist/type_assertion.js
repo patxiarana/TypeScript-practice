0283f3771618fbdd2322b43b76a37f473f34ee37
patxi <patxiarana05@gmail.com>
2023-02-12 09:09:11 -0300

    functions tsc

---
+++ dist/type_assertion.js
@@ -0,0 +1,7 @@
+"use strict";
+// Type assertion 
+let channel = 'patxidev';
+//let channelStr = <string> channel ;
+let channelStr = channel;
+// const myCanvas = document.getElementById('main') as HTMLCanvasElement ;
+const myCanvas = document.getElementById('main');
---
+++ src/functions.ts
@@ -0,0 +1,7 @@
+
+function greet (name: string) {
+    console.log(`hello..${name.toUpperCase()} !!`)
+}
+
+
+greet('true')
