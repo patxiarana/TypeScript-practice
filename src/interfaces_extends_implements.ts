f1e32fc6612394e137b21a8b4ba520525d3fe06d
patxi <patxiarana05@gmail.com>
2023-02-20 18:59:05 -0300

    applying extends

--- src/interfaces_extends_implements.ts
+++ src/interfaces_extends_implements.ts
@@ -6,16 +6,13 @@ interface Person {
     name:string;
     }
 
-interface Employee {
-id : number;
-name:string;
+interface Employee extends Person  {
 dept: string ;
 }
 
 
 
-interface Customer {
-    id : number;
-    name:string;
+interface Customer  extends Person{
     country: string ;
     }
+
