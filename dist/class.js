45f7f8aa1efdbef1c084ffb7a49a89677854600c
patxi <patxiarana05@gmail.com>
2023-02-25 20:56:26 -0300

    compiling

--- dist/class.js
+++ dist/class.js
@@ -1,1 +1,11 @@
 "use strict";
+class Employee {
+    constructor() {
+        this.showInfo();
+    }
+    //Metodos 
+    showInfo() {
+        console.log(`${this.name}`);
+    }
+}
+const emp = new Employee();
